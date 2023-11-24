3fea3a1aa1b61bd7525e28dc566954c29f68526a
Tushar Barate <tushar.message88@gmail.com>
2023-11-24 10:31:15 +0530

    some css changes

--- src/features/newsFeed/NewsFeedItem.js
+++ src/features/newsFeed/NewsFeedItem.js
@@ -1,5 +1,5 @@
 import React, { memo } from "react"
-import Col from "react-bootstrap/esm/Col"
+import Col from "react-bootstrap/Col"
 import Card from 'react-bootstrap/Card';
 import { NEWS_FEED_META } from "../../utils/constants";
 import { travereseNestedObject } from "../../utils/utils";
@@ -16,13 +16,13 @@ const NewsFeedItem = ({article, currentNFSource}) => {
     let title = Array.isArray(mapperObj['title']) ? travereseNestedObject(article, mapperObj['title']) : article[mapperObj['title']]
     
     return (
-        <Col sm>
+        <Col sm className="news_feed">
             <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={siteImageUrl+imageToUrl} />
+                <Card.Img variant="top" src={siteImageUrl+imageToUrl} height="300" className="news_feed__image"/>
                 <Card.Body>
-                    <Card.Title>{title}</Card.Title>
+                    <Card.Title>{title.substring(0,40)}...</Card.Title>
                     <Card.Text>
-                        {article[mapperObj['content']]}
+                        {article[mapperObj['content']].substring(0, 80)}...
                     </Card.Text>
                     
                     <Link to={ article[mapperObj['webUrl']]} target="_blank" >
